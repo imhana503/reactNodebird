6052792a58f12982ac7db10893c5104960f5f12b
Lim Hana <imhana503@gmail.com>
2025-08-18 05:23:46 +0900

    포스트이미지

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,6 +1,44 @@
-const PostImages = () => {
+import React, { useState, useCallback } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
+import PropTypes from 'prop-types';
+
+const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
+  const onZoom = useCallback(()=>{
+
+  }, []);
+
+  if(images.length === 1){
+    return(
+      <>
+        <img src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+      </>
+    )
+  } else if(images.length === 2){
+    return(
+      <>
+        <img src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+        <img src={images[1].src} alt={images[1].src} onClick={onZoom}/>
+      </>
+    )
+  }
   return(
-    <>구현중..</>
+    <>
+      <div>
+        <img role="presentation" width="50%" src={images[0].src} alt={images[0].src} onClick={onZoom}/>
+        <div 
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+          onClick={onZoom}
+        >
+          <PlusOutlined/>
+          <br/>
+          {images.length -1}
+          개의 사진 더보기
+
+        </div>
+      </div>
+    </>
   )
 }
 
